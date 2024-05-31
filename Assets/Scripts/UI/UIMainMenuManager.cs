ff047cbe9ac8d4b6cd68610fe60f46e3143b2c0d
nicorm2000 <nicorm2000@gmail.com>
2024-05-31 00:23:38 -0300

    Added level selection to main menu, added new models

--- Assets/Scripts/UI/UIMainMenuManager.cs
+++ Assets/Scripts/UI/UIMainMenuManager.cs
@@ -1,11 +1,46 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Job
+{
+    public string jobName;
+    public Sprite jobImage;
+    public string jobDescription;
+    public int jobPayment;
+    public int jobCompletionTimeEasy;
+    public int jobCompletionTimeMedium;
+    public int jobCompletionTimeHard;
+    public GameObject jobButton;
+}
 
 public class UIMainMenuManager : MonoBehaviour
 {
     [Header("Config")]
     [SerializeField] private TextMeshProUGUI gameTime = null;
+    [SerializeField] private Job[] jobAvailables = null;
+    [SerializeField] private Image jobImage = null;
+    [SerializeField] private TextMeshProUGUI jobNameText = null;
+    [SerializeField] private TextMeshProUGUI jobDescriptionText = null;
+
+    [Header("Navigation Buttons")]
+    [SerializeField] private Button leftButton = null;
+    [SerializeField] private Button rightButton = null;
+
+    private int currentIndex = 0;
+
+    private void Awake()
+    {
+        leftButton.onClick.AddListener(NavigateLeft);
+        rightButton.onClick.AddListener(NavigateRight);
+    }
+
+    private void Start()
+    {
+        UpdateJob();
+    }
 
     private void Update()
     {
@@ -17,5 +52,31 @@ public class UIMainMenuManager : MonoBehaviour
         DateTime now = DateTime.Now;
         string formattedTime = now.ToString("hh:mm tt");
         gameTime.text = formattedTime;
+    }
+
+    private void NavigateRight()
+    {
+        currentIndex = (currentIndex + 1) % jobAvailables.Length;
+        UpdateJob();
+    }
+
+    private void NavigateLeft()
+    {
+        currentIndex = (currentIndex - 1 + jobAvailables.Length) % jobAvailables.Length;
+        UpdateJob();
+    }
+
+    private void UpdateJob()
+    {
+        if (jobAvailables.Length == 0) return;
+
+        jobNameText.text = jobAvailables[currentIndex].jobName;
+        jobImage.sprite = jobAvailables[currentIndex].jobImage;
+        jobDescriptionText.text = jobAvailables[currentIndex].jobDescription;
+
+        for (int i = 0; i < jobAvailables.Length; i++)
+        {
+            jobAvailables[i].jobButton.SetActive(i == currentIndex);
+        }
     }
 }
