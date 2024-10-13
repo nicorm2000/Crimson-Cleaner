b6384d2b8f9427bab77b85fc0a43e6395c832c64
nicorm2000 <nicorm2000@gmail.com>
2024-10-13 16:37:28 -0300

    Added new sanity audios 69%

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,9 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BABY_CRYING = 2147733524U;
         static const AkUniqueID BAR_DOOR = 2533397739U;
+        static const AkUniqueID BAR_FIGHT = 3295692891U;
         static const AkUniqueID BAT__COLLISION = 3932140208U;
         static const AkUniqueID BATHROOM_DOOR = 1431659638U;
         static const AkUniqueID BLOODPOOL_FOOTSTEP_PLAY = 3337018563U;
@@ -32,6 +34,7 @@ namespace AK
         static const AkUniqueID CHAIR_COLLISION = 3537521357U;
         static const AkUniqueID CLOSE_NOTEBOOK = 4085905093U;
         static const AkUniqueID COUCH_CHAIR_COLLISION = 910731106U;
+        static const AkUniqueID COUPLE_FIGHT = 3987390466U;
         static const AkUniqueID CUBICLE_BATHROOM_DOOR = 3141789928U;
         static const AkUniqueID DOBILLIARD_BALL_COLLISION = 636776174U;
         static const AkUniqueID DRAWER_CLOSE = 3753111523U;
@@ -39,6 +42,7 @@ namespace AK
         static const AkUniqueID DROP = 1878686274U;
         static const AkUniqueID DROP_BOILING_WATER = 1925664349U;
         static const AkUniqueID FIRE_CART = 1357891606U;
+        static const AkUniqueID FOOTSTEPS_RUN = 2462760816U;
         static const AkUniqueID FRAME_COLLISION = 3611395209U;
         static const AkUniqueID FRIDGE_CIOSE = 2736826332U;
         static const AkUniqueID FRIDGE_OPEN = 2916123785U;
@@ -60,6 +64,7 @@ namespace AK
         static const AkUniqueID MACHETE_COLLISION = 2997531735U;
         static const AkUniqueID MEDIUM_METAL_TOOL_COLLISION = 579873924U;
         static const AkUniqueID MENU_COLLISION = 295281277U;
+        static const AkUniqueID MESSAGE_NOTIFICATION = 3090986788U;
         static const AkUniqueID MONEY = 758863303U;
         static const AkUniqueID MOP = 1082004779U;
         static const AkUniqueID MOP_DIRTY = 68175526U;
@@ -68,15 +73,18 @@ namespace AK
         static const AkUniqueID OBJECT_CLEANED = 836873265U;
         static const AkUniqueID OBJECT_PLACEMENT_CONFIRMATION = 1967001744U;
         static const AkUniqueID OPEN_NOTEBOOK = 1673104519U;
+        static const AkUniqueID PHONE_RING = 3401439988U;
         static const AkUniqueID PICK_UP = 467838998U;
         static const AkUniqueID PICK_UP_TRASH = 3160723937U;
         static const AkUniqueID PLATE_BREAKING = 2970993869U;
         static const AkUniqueID POKER_CHIP_COLLISION = 3961112132U;
+        static const AkUniqueID POLICE_SIREN = 2102979017U;
         static const AkUniqueID POOL_STICK_COLLISION = 3176396303U;
         static const AkUniqueID REMOVE_TRASH = 2748915226U;
         static const AkUniqueID RETRIEVE_CLOTHES = 2786157920U;
         static const AkUniqueID RETRIEVE_DOCUMENTS = 2186617010U;
         static const AkUniqueID RETRIEVE_ROPE = 362869116U;
+        static const AkUniqueID REVOLVER_COCK = 1351538419U;
         static const AkUniqueID ROPE_COLLISION = 3225123216U;
         static const AkUniqueID SAFE_OPEN_CLOSE = 2038894488U;
         static const AkUniqueID SELECT_TRASH_BIN = 1254856804U;
@@ -98,6 +106,7 @@ namespace AK
         static const AkUniqueID WATER_FLOW_STOP = 1430150536U;
         static const AkUniqueID WATER_TAP = 2864239532U;
         static const AkUniqueID WEAPON_SWAP = 2239924753U;
+        static const AkUniqueID WHISPERS = 2628386460U;
         static const AkUniqueID WOOD_FOOTSTEP_PLAY = 1523901220U;
         static const AkUniqueID WOOD_FOOTSTEP_STOP = 1381150U;
     } // namespace EVENTS
