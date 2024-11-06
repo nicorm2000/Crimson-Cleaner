fbf9fa74e7d95b90fa687e57e23fa8a505970f43
nicorm2000 <nicorm2000@gmail.com>
2024-11-05 21:22:23 -0300

    Added credits to lobby

--- Assets/Scripts/UI/Credits UI Manager.cs
+++ Assets/Scripts/UI/Credits UI Manager.cs
@@ -6,14 +6,37 @@ public class CreditsUIManager : MonoBehaviour
     [Header("Config")]
     [SerializeField] private GameObject creditsTab = null;
     [SerializeField] private Button backToLobbyButton = null;
+    [SerializeField] private RectTransform creditsText = null;
+    [SerializeField] private float duration = 0;
+    [SerializeField] private float startY = 0;
+    [SerializeField] private float endY = 0;
 
     [Header("Audio Config")]
     [SerializeField] private AudioManager audioManager = null;
     [SerializeField] private string clickEvent = null;
 
+    private float elapsedTime;
+
     private void Awake()
     {
         backToLobbyButton.onClick.AddListener(() => { OpenTab(creditsTab, false); });
+    }
+
+    private void OnEnable()
+    {
+        elapsedTime = 0f;
+        creditsText.anchoredPosition = new Vector2(creditsText.anchoredPosition.x, startY);
+    }
+
+    private void Update()
+    {
+        if (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float progress = elapsedTime / duration;
+            float newY = Mathf.Lerp(startY, endY, progress);
+            creditsText.anchoredPosition = new Vector2(creditsText.anchoredPosition.x, newY);
+        }
     }
 
     private void OpenTab(GameObject go, bool state)
